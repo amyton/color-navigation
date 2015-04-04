2add9c814af21c9464c4152ad4364d3b8eb40657
Amy Ton <amyhton@gmail.com>
2015-04-04 16:32:17 -0500

    change height values and click glitch sorta works

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var isClicked = false;
+
 $('nav li').click(function() {
   $('nav li').css('background', 'none');
   $('nav li').removeClass('selected');
@@ -19,6 +21,11 @@ $('nav li').click(function() {
   var positionFromTop = $('.color-wrapper .color-' + colorName).offset().top;
   $('body').animate({ scrollTop: (positionFromTop + 1) + 'px' });
 
+  isClicked = true;
+
+  setTimeout(function() {
+    isClicked = false;
+  }, 1000);
 });
 
 console.log('Get all .color DOM elements:');
@@ -28,13 +35,15 @@ $('.color').each(function() {
   console.log("Loop through all the DOM elements:");
   console.log($(this));
 
-  $(this).height('600px');
+  $(this).height('800px');
   var color = $(this).data('color');
   $(this).css('background', color);
 })
 
-$(document).scroll(function () {
-  updateColorSelection();
+$(document).scroll(function() {
+  if (!isClicked) {
+    updateColorSelection();
+  }
 });
 
 $(document).ready(function () {
@@ -47,25 +56,25 @@ function updateColorSelection () {
   $('nav li').css('background', 'none');
   $('nav li').removeClass('selected');
 
-  if (positionFromTop <= 300) {
+  if (positionFromTop <= 400) {
     $('.color-name').html("red");
     $('li.color-red').css('background', 'red').addClass('selected');
-  } else if (positionFromTop <= 1000) {
+  } else if (positionFromTop <= 1200) {
     $('.color-name').html("orange");
     $('li.color-orange').css('background', 'orange').addClass('selected');
-  } else if (positionFromTop <= 1500) {
+  } else if (positionFromTop <= 2100) {
     $('.color-name').html("yellow");
     $('li.color-yellow').css('background', 'yellow').addClass('selected');
-  } else if (positionFromTop <= 2000) {
+  } else if (positionFromTop <= 2900) {
     $('.color-name').html("green");
     $('li.color-green').css('background', 'green').addClass('selected');
-  } else if (positionFromTop <= 2500) {
+  } else if (positionFromTop <= 3700) {
     $('.color-name').html("blue");
     $('li.color-blue').css('background', 'blue').addClass('selected');
-  } else if (positionFromTop <= 3000) {
+  } else if (positionFromTop <= 4400) {
     $('.color-name').html("indigo");
     $('li.color-indigo').css('background', 'indigo').addClass('selected');
-  } else if (positionFromTop <= 3500) {
+  } else if (positionFromTop <= 5000) {
     $('.color-name').html("violet");
     $('.color-violet').css('background', 'violet').addClass('selected');
   }
